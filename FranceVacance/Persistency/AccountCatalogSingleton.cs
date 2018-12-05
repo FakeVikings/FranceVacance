9958af55591b67aab033695647350c9adf06e3a9
Tomas Vemola <vemolista@gmail.com>
2018-12-05 16:46:10 +0100

    Refactoring - added VerifyAccount class to separate resposibility

--- FranceVacance/Persistency/AccountCatalogSingleton.cs
+++ FranceVacance/Persistency/AccountCatalogSingleton.cs
@@ -16,10 +16,12 @@ namespace FranceVacance.Persistency
     {
         public List<Account> AccountList { get; set; }
         private static AccountCatalogSingleton _instance;
+        private VerifyAccount _verifyAccount;
 
         private AccountCatalogSingleton()
         {
             AccountList = new List<Account>() {new Account (fullname:"Tomas Vemola",email:"tomas@project.com",password:"Verystrongpassword1")};
+            _verifyAccount = new VerifyAccount();
         }
 
         public static AccountCatalogSingleton Instance
@@ -40,86 +42,86 @@ namespace FranceVacance.Persistency
             AccountList.Add(account);
             MessageBox.Success("Account has been created.");
         }
-        public bool NullExeption(string fullname, string email, string password)
-        {
-            if (fullname != null && email != null && password != null)
-            {
-                return true;
-            }
-            else{
-                MessageBox.Fail("Please fill in all the columns.");
-                return false;
-            }
-        }
-        public bool VerifyAccount(string fullname, string email, string password)
-        {
-            if (AccountList.Exists(a => a.Email == email))
-            {
-                MessageBox.Fail("This Email is already in use.");
-                return false;
-            }
-            if (fullname.Length < 3)
-            {
-                MessageBox.Fail("Fullname must have more than 3 chars");
-                return false;
-            }
-            if (!(email.Contains("@") && email.Contains(".")))
-            {
-                MessageBox.Fail("Email is not valid");
-                return false;
-            }
-            if (password.Length < 8
-                ||
-                !(password.Any(char.IsUpper)) ||
-                !(password.Any(char.IsLower)) ||
-                !(password.Any(char.IsDigit)))
-            {
-                MessageBox.Fail("Password must have at least 8 characters, one uppercase letter, one lowercase letter and one number.");
-                return false;
-            }
-            {
-                return true;
-            }
-        }
+        //public bool NullExeption(string fullname, string email, string password)
+        //{
+        //    if (fullname != null && email != null && password != null)
+        //    {
+        //        return true;
+        //    }
+        //    else{
+        //        MessageBox.Fail("Please fill in all the columns.");
+        //        return false;
+        //    }
+        //}
+        //public bool VerifyAccount(string fullname, string email, string password)
+        //{
+        //    if (AccountList.Exists(a => a.Email == email))
+        //    {
+        //        MessageBox.Fail("This Email is already in use.");
+        //        return false;
+        //    }
+        //    if (fullname.Length < 3)
+        //    {
+        //        MessageBox.Fail("Fullname must have more than 3 chars");
+        //        return false;
+        //    }
+        //    if (!(email.Contains("@") && email.Contains(".")))
+        //    {
+        //        MessageBox.Fail("Email is not valid");
+        //        return false;
+        //    }
+        //    if (password.Length < 8
+        //        ||
+        //        !(password.Any(char.IsUpper)) ||
+        //        !(password.Any(char.IsLower)) ||
+        //        !(password.Any(char.IsDigit)))
+        //    {
+        //        MessageBox.Fail("Password must have at least 8 characters, one uppercase letter, one lowercase letter and one number.");
+        //        return false;
+        //    }
+        //    {
+        //        return true;
+        //    }
+        //}
         public void CreateAccount(string fullname, string email, string password)
         {
-            if (NullExeption(fullname, email, password) && VerifyAccount(fullname, email, password))
+            if (_verifyAccount.VerifyNewAccount(fullname, email, password, AccountList))
             {
                 Account account = new Account(fullname, email, password) { Fullname = fullname, Email = email, Password = password };
                 AddAccount(account);
             }
         }
-        public bool NullExeption1(string email, string password)
-        {
-            if (email != null && password != null)
-            {
-                return true;
+        //public bool NullExeption1(string email, string password)
+        //{
+        //    if (email != null && password != null)
+        //    {
+        //        return true;
 
-            }
-            {
-                MessageBox.Fail("Please fill both columns");
-                return false;
-            }
-        }
-        public Account LoginVerification(string email, string password)
-        {
-            if (AccountList.Exists(a => a.Email == email))
-            {
-                var accountToLogIn = AccountList.Find(a => a.Email == email);
-                if (accountToLogIn.Password == password)
-                {
-                    return accountToLogIn;
-                }
+        //    }
+        //    {
+        //        MessageBox.Fail("Please fill both columns");
+        //        return false;
+        //    }
+        //}
+        //public Account LoginVerification(string email, string password)
+        //{
+        //    if (AccountList.Exists(a => a.Email == email))
+        //    {
+        //        var accountToLogIn = AccountList.Find(a => a.Email == email);
+        //        if (accountToLogIn.Password == password)
+        //        {
+        //            return accountToLogIn;
+        //        }
 
-                return null;
-            }
+        //        return null;
+        //    }
 
-            return null;
-        }
+        //    return null;
+        //}
     
         public void GainAccess(string email, string password)
         {
-            if (LoginVerification(email, password) != null)
+            if (_verifyAccount.VerifyExistingAccount(email, password, AccountList) != null)
             {
                 LoginSuccessful();
             }
---
+++ FranceVacance/Persistency/VerifyAccount.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FranceVacance.Model;
+using FranceVacance.ViewModel;
+
+namespace FranceVacance.Persistency
+{
+    class VerifyAccount
+    {
+        private bool IsInputEmpty(string fullname, string email, string password)
+        {
+            return fullname == null || email == null || password == null;
+        }
+
+        public bool VerifyNewAccount(string fullname, string email, string password, List<Account> accountList)
+        {
+            if (IsInputEmpty(fullname, email, password) == true)
+            {
+                MessageBox.Fail("Please fill in all boxes.");
+                return false;
+            }
+            if (accountList.Exists(a => a.Email == email))
+            {
+                MessageBox.Fail("This email is already in use.");
+                return false;
+            }
+
+            if (fullname.Length < 3)
+            {
+                MessageBox.Fail("Fullname must have more than 3 characters.");
+                return false;
+            }
+
+            if (!(email.Contains("@") && email.Contains(".")))
+            {
+                MessageBox.Fail("Email must contain \"@\" and \".\"");
+                return false;
+            }
+
+            if (
+                password.Length < 8
+                || !(password.Any(char.IsUpper))
+                || !(password.Any(char.IsLower))
+                || !(password.Any(char.IsDigit))
+            )
+            {
+                MessageBox.Fail("Password must have at least 8 characters, one uppercase letter, one lowercase letter and one number.");
+                return false;
+            }
+
+            return true;
+        }
+
+        public Account VerifyExistingAccount(string email, string password, List<Account> accountList)
+        {
+            if (accountList.Exists(a => a.Email == email))
+            {
+                var accountToLogIn = accountList.Find(a => a.Email == email);
+                if (accountToLogIn.Password == password)
+                {
+                    return accountToLogIn;
+                }
+
+                return null;
+            }
+
+            return null;
+        }
+    }
+}
