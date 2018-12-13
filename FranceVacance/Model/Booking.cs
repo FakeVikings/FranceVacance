968c3b775c0751f30c328244ea564639a80f465c
Thatsoneking <bori0408@edu.easj.dk>
2018-12-13 13:47:27 +0100

    Booking added{shitty one}

---
+++ FranceVacance/Model/Booking.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FranceVacance.Model
+{
+    public class Booking
+    {
+        public int Price { get; set; }
+        public bool IsPaid { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public Accomodation Accomodation{ get ; set ; }
+
+        public List<Booking> BookingList { get; set; }
+        private static Booking _instance;
+
+        public Booking( int price,bool IsPaid, DateTime Start, DateTime End,string Accomodation)
+        {
+            Price = price;
+            IsPaid = false;
+        }
+
+        private Booking()
+        {
+        }
+    }
+}
