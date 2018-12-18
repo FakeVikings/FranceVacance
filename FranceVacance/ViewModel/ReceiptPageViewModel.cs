1cb44382b63edcae2d0ee798b5786c6b9014f140
Tomas Vemola <vemolista@gmail.com>
2018-12-18 07:09:39 +0100

    Refactoring

--- FranceVacance/ViewModel/ReceiptPageViewModel.cs
+++ FranceVacance/ViewModel/ReceiptPageViewModel.cs
@@ -31,16 +31,6 @@ namespace FranceVacance.ViewModel
             _currentBooking = _bookingCatalogSingleton.CurrentBooking;
             GoMainPageCommand = new RelayCommand(GoMainPage);
             GoMyBookingPageCommand = new RelayCommand(GoMyBookingPage);
-            /*
-            Fullname = _bookingCatalogSingleton.CurrentBooking.User.Fullname;
-            Email = _bookingCatalogSingleton.CurrentBooking.User.Email;
-            Country = _bookingCatalogSingleton.CurrentBooking.Accommodation.Country;
-            City = _bookingCatalogSingleton.CurrentBooking.Accommodation.City;
-            PricePerNight = _bookingCatalogSingleton.CurrentBooking.Accommodation.PricePerNight;
-            StartDate = _bookingCatalogSingleton.CurrentBooking.StartDate;
-            EndDate = _bookingCatalogSingleton.CurrentBooking.EndDate;
-            Price = _bookingCatalogSingleton.CurrentBooking.Price;
-            */
         }
 
         public Booking CurrentBooking
@@ -53,87 +43,6 @@ namespace FranceVacance.ViewModel
             }
         }
 
-        /*
-        public string Fullname
-        {
-            get { return _fullname; }
-            set
-            {
-                _fullname = value;
-                OnPropertyChanged("Fullname");
-            }
-        }
-
-        public string Email
-        {
-            get => _email;
-            set
-            {
-                _email = value;
-                OnPropertyChanged("Email");
-            }
-        }
-
-        public string Country
-        {
-            get => _country;
-            set
-            {
-                _country = value;
-                OnPropertyChanged("Country");
-            }
-        }
-
-        public string City
-        {
-            get => _city;
-            set
-            {
-                _city = value;
-                OnPropertyChanged("City");
-            }
-        }
-
-        public int PricePerNight
-        {
-            get => _pricePerNight;
-            set
-            {
-                _pricePerNight = value;
-                OnPropertyChanged("PricePerNight");
-            }
-        }
-
-        public DateTimeOffset StartDate
-        {
-            get => _startDate;
-            set
-            {
-                _startDate = value;
-                OnPropertyChanged("StartDate");
-            }
-        }
-
-        public DateTimeOffset EndDate
-        {
-            get => _endDate;
-            set
-            {
-                _endDate = value;
-                OnPropertyChanged("EndDate");
-            }
-        }
-
-        public int Price
-        {
-            get => _price;
-            set
-            {
-                _price = value;
-                OnPropertyChanged("Price");
-            }
-        }
-        */
         public void GoMainPage()
         {
             Navigate.ActivateFrameNavigation(typeof(MainPage));
